22be15676223181010e30410dad34f8194058173
NNYFC <nfrnanckcesar@gmail.com>
2024-05-27 19:27:05 +0100

    to Object add and list using

--- homepage.js
+++ homepage.js
@@ -4,4 +4,20 @@
  var modal = document.getElementById("submitBtn");
 
 
-  
+const todo = {
+    name: "web notions",
+    description: "This is to learn basic web development",
+    date: new Date(),
+    status: "PENDING"
+};
+
+
+function addTodo(){
+    todo.name = document.getElementById("todoName").value;
+    document.getElementById("name").innerHTML = todo.name;
+    todo.description = document.getElementById("todoDescription").value;
+    document.getElementById("description").innerHTML = todo.description;
+    todo.date = document.getElementById("todoDate").value;
+    document.getElementById("date").innerHTML = todo.date;
+    document.getElementById("status").innerHTML = todo.status;
+}
