147883257ad062556cf846326a91b26e025af16c
kingderick55 <165064725+kingderick55@users.noreply.github.com>
2024-05-31 13:04:09 +0200

    new comit

---
+++ homepage.js
@@ -0,0 +1,7 @@
+// 1st method of creating a js object
+var student = {
+    name = "derick";
+    age = 23 ;
+    studies = "webdevelopement" ;
+}
+document.getElementById("word") = student.name + "of the name" +  student.age + "of the age" + student.studies + "studies" ;
