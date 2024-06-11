15413a4450c7c6543db7efc0a2265d6120ffc127
kingderick55 <165064725+kingderick55@users.noreply.github.com>
2024-06-11 20:08:20 +0200

    this is my comit

---
+++ homepage.js
@@ -0,0 +1,28 @@
+// create a variable to get the modal and other elements of the modal
+var modal = document.getElementById("exampleModal");
+var modal = document.getElementById("addElementForm");
+var modal = document.getElementById("submitBtn");
+
+
+const todo = {
+   name: "web notions",
+   description: "This is to learn basic web development",
+   date: new Date(),
+   status: "PENDING"
+};
+
+var todos = [];
+
+
+function addTodo(){
+   todo.name = document.getElementById("todoName").value;
+   // document.getElementById("name").innerHTML = todo.name;
+   todo.description = document.getElementById("todoDescription").value;
+   // document.getElementById("description").innerHTML = todo.description;
+   todo.date = document.getElementById("todoDate").value;
+   // document.getElementById("date").innerHTML = todo.date;
+   // document.getElementById("status").innerHTML = todo.status;
+   todos.push(todo)
+   var demo = document.getElementById("todoList");
+   todos.forEach(todo => demo.innerHTML += "<br>" + Object.values(todo).join(" "));
+   
