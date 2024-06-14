d8edbdcaebfa3df48c4f7dd4495a93191f29786a
NNYFC <nfrnanckcesar@gmail.com>
2024-06-14 19:09:54 +0100

    sql script modified

--- SQL_script.sql
+++ SQL_script.sql
@@ -1,11 +1,11 @@
 -- SQL query to create database
 CREATE DATABASE schooltask;
 -- SQL queries to create table
-CREATE TABLE subject(id INT NOT NULL auto_increment, name VARCHAR(30) NOT NULL, PRIMARY KEY(id));
-CREATE TABLE students( id INT NOT NULL auto_increment , name VARCHAR(30) not null, primary key(id));
+CREATE TABLE schooltask.subject(id INT NOT NULL auto_increment, name VARCHAR(30) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE schooltask.students( id INT NOT NULL auto_increment , name VARCHAR(30) not null, primary key(id));
 -- SQL query to insert data in table of a database
-INSERT INTO subject(name) VALUES("ANDROID Programming");
+INSERT INTO schooltask.subject(name) VALUES("ANDROID Programming");
 -- SQL query to display all table attributes and their records of a database
-SELECT * FROM subject;
+SELECT * FROM schooltask.subject;
 -- SQL query to delete a database
 DROP DATABASE schooltask;
