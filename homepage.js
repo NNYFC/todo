06ae685007f1b1097d1f56264444dc1589e558e5
NNYFC <nfrnanckcesar@gmail.com>
2024-06-14 19:01:41 +0100

    todo listing page

--- homepage.js
+++ homepage.js
@@ -11,13 +11,22 @@ const todo = {
     status: "PENDING"
 };
 
+var todos = [];
+
 
 function addTodo(){
     todo.name = document.getElementById("todoName").value;
-    document.getElementById("name").innerHTML = todo.name;
+    // document.getElementById("name").innerHTML = todo.name;
     todo.description = document.getElementById("todoDescription").value;
-    document.getElementById("description").innerHTML = todo.description;
+    // document.getElementById("description").innerHTML = todo.description;
     todo.date = document.getElementById("todoDate").value;
-    document.getElementById("date").innerHTML = todo.date;
-    document.getElementById("status").innerHTML = todo.status;
+    // document.getElementById("date").innerHTML = todo.date;
+    // document.getElementById("status").innerHTML = todo.status;
+    todos.push(todo)
+    var demo = document.getElementById("todoList");
+    // todos.forEach(todo => demo.innerHTML += "<br>" + Object.values(todo).join(" "));
+    todos.forEach(todo => {
+        demo.innerHTML += "<td>" + todo.name + "</td>" + "<td>" + todo.description + "</td>" +
+         "<td>" + todo.date + "</td>" + "<td>" + todo.status + "</td>" + "<br>"
+    });
 }
